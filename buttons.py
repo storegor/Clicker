0814dc178ef7587df85c22c629e9284580dc1765
storegor <99485534+storegor@users.noreply.github.com>
2022-05-01 21:20:30 +0300

    закончил рефакторинг + цена на улучшение автоклика растет с увеличением автоклика

--- buttons.py
+++ buttons.py
@@ -8,9 +8,9 @@ class BtcButton:
         self.place_ = (360, 450)
         self.click_zone_ = (410, 500)
         self.screen_ = screen
+        self.target_ = self.btc_surf_.get_rect(centerx=self.click_zone_[0], centery=self.click_zone_[1])
 
     def draw_button(self):
-        self.btc_surf_.get_rect(centerx=self.click_zone_[0], centery=self.click_zone_[1])
         self.screen_.blit(self.btc_surf_, self.place_)
 
 
@@ -22,10 +22,11 @@ class ImproveButton:
         self.red_ = (255, 0, 0)
         self.white_ = (255, 255, 255)
         self.screen_ = screen
+        self.target_ = self.surf_.get_rect(x=self.w_sell_ // 2 + 3 * self.w_sell_ + 20, y=self.h_sell_ + 1)
+
 
     def draw_button(self):
         self.surf_.fill(self.red_)
-        self.surf_.get_rect(x=self.w_sell_ // 2 + 3 * self.w_sell_ + 20, y=self.h_sell_ + 1)
-        text_improve_btc_auto_click = pygame.font.SysFont('arial', 17).render('improve autoclick', False, self.white_, None)
-        self.screen_.blit(self.surf_, (self.w_sell_ // 2 + 3 * self.w_sell_ + 2, self.h_sell_))
-        self.screen_.blit(text_improve_btc_auto_click, (self.w_sell_ // 2 + 3 * self.w_sell_ + 5, self.h_sell_))
+        text_improve_btc_auto_click = pygame.font.SysFont('arial', 17).render('update autoclick', False, self.white_, None)
+        self.screen_.blit(self.surf_, (self.w_sell_ // 2 + 4 * self.w_sell_, self.h_sell_))
+        self.screen_.blit(text_improve_btc_auto_click, (self.w_sell_ // 2 + 4 * self.w_sell_ + 5, self.h_sell_))
